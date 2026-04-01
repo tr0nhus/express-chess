8a3dd7f4cd239518671a604f7941f0bf1f58c785
Magnus T <magnus.tronhus@gmail.com>
2026-04-02 00:58:49 +0200

    Express server + chess UI and movement logic

---
+++ node/app.js
@@ -0,0 +1,16 @@
+import express from "express";
+import { fileURLToPath } from "url";
+import { dirname, join } from "path";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const app = express();
+
+const port = 3000;
+
+app.use(express.static(join(__dirname, "public")));
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
